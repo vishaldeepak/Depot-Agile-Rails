00ea78c280595fe3247bf7ecddf8d396c188bbe6
VISHAL DEEPAK <vishal.deepak@outlook.com>
2015-05-17 10:09:01 +0530

    small changes on chapter 14 itertion 1

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
     mail = OrderNotifier.received(morders(:one))
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    #assert_match "Hi", mail.body.encoded
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["team@depot.com"], mail.from
+    #assert_match "Hi", mail.body.encoded #Later
   end
 
   test "shipped" do
     mail = OrderNotifier.shipped(morders(:one))
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    #assert_match "Hi", mail.body.encoded
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["team@depot.com"], mail.from
+    #assert_match "Hi", mail.body.encoded #later
   end
 
 end
